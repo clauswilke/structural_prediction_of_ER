8019b02c6beb4db75231e4108afc61b38680d1be
Amir Shahmoradi <a.shahmoradi@gmail.com>
2014-01-31 16:26:25 -0600

    some figures and files added

--- correlation_analysis/src/cor_entropy_fluctuations.r
+++ correlation_analysis/src/cor_entropy_fluctuations.r
@@ -55,7 +55,7 @@ for(protein in levels(data$protein))
     
     row = data.frame( protein=protein,
                       phi_rho = r.phi_var_md, phi_P = p.phi_var_md, psi_rho = r.psi_var_md, psi_P = p.psi_var_md, chi1_rho = r.chi1_var_md, chi1_P = p.chi1_var_md,
-                      avg_md_rho = r.rmsf_avg_md, avg_md_P = p.rmsf_avg_md, bfca_rho = r.bfca, bfca_P = p.bfca, avg_cr_rho = r.rmsf_cr, avg_cr_P = p.rmsf_cr ) # , std_md_rho = r.rmsf_std_md, std_md_P = p.rmsf_std_md
+                      bfca_rho = r.bfca, bfca_P = p.bfca, avg_md_rho = r.rmsf_avg_md, avg_md_P = p.rmsf_avg_md, avg_cr_rho = r.rmsf_cr, avg_cr_P = p.rmsf_cr ) # , std_md_rho = r.rmsf_std_md, std_md_P = p.rmsf_std_md
     result = rbind( result, row )
     print( protein )
 }
@@ -63,23 +63,24 @@ row.names(result) = c()
 write.csv( result, "correlation_analysis/cor_tables/cor_entropy_SF.csv", row.names=F )
 
 
-index = names(result) %in% c("phi_rho", "psi_rho", "chi1_rho", "avg_md_rho", "bfca_rho", "avg_cr_rho") # columns we want to plot
-index.p = names(result) %in% c("phi_P", "psi_P", "chi1_P", "avg_cr_P", "avg_md_P", "bfca_P") # columns that store significance
+index = names(result) %in% c("phi_rho", "psi_rho", "chi1_rho", "bfca_rho", "avg_md_rho", "avg_cr_rho") # columns we want to plot
+index.p = names(result) %in% c("phi_P", "psi_P", "chi1_P", "bfca_P", "avg_md_P", "avg_cr_P") # columns that store significance
 
 colors = c('red', 'blue', 'green', 'purple', 'chocolate1', 'darkgreen', 'black', 'gray', 'cyan2') #, 'darkred', 'darkgreen', 'bisque2')
 proteins = c('1RD8_AB', '2FP7_B', '2JLY_A', '2Z83_A', '3GOL_A', '3LYF_A', '4AQF_B', '4GHA_A', '4IRY_A') #, '3GSZ_A', '3I5K_A', '2JLY_A_temp_50', '2JLY_A_temp_100', '2JLY_A_temp_200', '2JLY_A_temp_450')
 labels = c('1RD8', '2FP7', '2JLY', '2Z83', '3GOL', '3LYF', '4AQF', '4GHA', '4IRY') #, '3GSZ_A', '3I5K_A', '2JLY_A_temp_50', '2JLY_A_temp_100', '2JLY_A_temp_200', '2JLY_A_temp_450')
 
-pdf( "correlation_analysis/figures/cor_entropy_SF.pdf", width=9, height=4, useDingbats=FALSE )
-par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab=expression(paste("Correlation (", rho, ") with entropy")),xlab='Correlating Variable', xlim=c(1,6.2),ylim=c(-.5,.5)) #, main = 'sequence entropy - contact number association', cex.main=0.8)
+pdf( "correlation_analysis/figures/cor_entropy_SF.pdf", width=7, height=4, useDingbats=FALSE )
+par( mai=c(0.65, 0.65, 0.0, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
+plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab=expression(paste("Correlation (", rho, ") with entropy")),xlab='Correlating Variable', xlim=c(1,6.2),ylim=c(-.5,.45)) #, main = 'sequence entropy - contact number association', cex.main=0.8)
 #minor.tick(nx=0, ny=4, tick.ratio=2)
 axis( 2,  # y axis
 	  at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
 	  c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
 axis( 1,
       at=c(1, 2, 3, 4, 5, 6),
-      c(expression(paste("variance ", phi)), expression(paste("variance ", psi)), expression(paste("variance ", chi[1])), "RMSF (MD)", expression(paste("B-factor @ C", alpha)), "RMSF (CS)"))
+      padj=c(0,0,0,0,0,0),
+      c(expression(paste("variance ", phi)), expression(paste("variance ", psi)), expression(paste("variance ", chi[1])), "bfactor", "MD rmsf", "CS rmsf"))
 
 
 for( i in 1:nrow(result) )
