3eed561e659090c998a61d601088834f10ac590b
Amir Shahmoradi <a.shahmoradi@gmail.com>
2014-02-01 18:04:35 -0600

    B factor VS. rmsf  figures and files added

--- correlation_analysis/src/cor_bfca_rmsf.r
+++ correlation_analysis/src/cor_bfca_rmsf.r
@@ -48,67 +48,37 @@ write.csv( result, "correlation_analysis/cor_tables/cor_bfca_rmsf.csv", row.name
 
 proteins = c('1RD8_AB', '2FP7_B', '2JLY_A', '2Z83_A', '3GOL_A', '3LYF_A', '4AQF_B', '4GHA_A', '4IRY_A') #, '3GSZ_A', '3I5K_A', '2JLY_A_temp_50', '2JLY_A_temp_100', '2JLY_A_temp_200', '2JLY_A_temp_450')
 
-pdf( "correlation_analysis/figures/cor_bfca_rmsf.pdf", width=9, height=8, useDingbats=FALSE )
+pdf( "correlation_analysis/figures/cor_bfca_rmsf.pdf", width=4.5, height=4, useDingbats=FALSE )
 
-split.screen(c(1,2))
+split.screen(c(1,1))
 
 screen(1)
 	par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="B factor",ylab="RMSF (MD simulation)", xlim=c(-.4,.4),ylim=c(-.4,.4))
+	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="B factor - RMSF (Homologous Structures)",ylab="B factor - RMSF (MD simulation)", xlim=c(0,.8),ylim=c(0,.8))
 	axis( 1,  # x axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
+		at=c(0.0,0.1,.2,0.3,0.4,0.5,0.6,0.7,0.8),
+		c("0.0","","0.2","","0.4","","0.6","","0.8"))
 	axis( 2,  # y axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
+		at=c(0.0,0.1,.2,0.3,0.4,0.5,0.6,0.7,0.8),
+		c("0.0","","0.2","","0.4","","0.6","","0.8"))
 	
 		
-	points( result$r.bfca_rmsfHS, result$r.rsa_avg_md, pch=19 )
+	points( result$r.bfca_rmsfHS, result$r.bfca_rmsfMD, pch=19 )
 	abline(0,1)
 
-screen(2)
-	par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="B factor",ylab="RMSF (Homologous Structures)", xlim=c(-.4,.4),ylim=c(-.4,.4))
-	axis( 1,  # x axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	axis( 2,  # y axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	
-		
-	points( result$r.rmsf_cr, result$r.rmsf_avg_md, pch=19 )
-	abline(0,1)
-	
-screen(3)
-	par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="entropy - CN13 (Crystal Structure)",ylab="entropy - CN13 (MD simulation)", xlim=c(-.4,.4),ylim=c(-.4,.4))
-	axis( 1,  # x axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	axis( 2,  # y axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	
-		
-	points( result$r.cn13_cr, result$r.cn13_avg_md, pch=19 )
-	abline(0,1)
-	
-screen(4)
-	par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="entropy - WCN (Crystal Structure)",ylab="entropy - WCN (MD simulation)", xlim=c(-.4,.4),ylim=c(-.4,.4))
-	axis( 1,  # x axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	axis( 2,  # y axis
-		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
-		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
-	
-		
-	points( result$r.wcn_cr, result$r.wcn_avg_md, pch=19 )
-	abline(0,1)
-
-
+#screen(2)
+#	par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
+#	plot(0,xaxt='n',yaxt='n',bty='n',pch='',xlab="B factor",ylab="RMSF (Homologous Structures)", xlim=c(-.4,.4),ylim=c(-.4,.4))
+#	axis( 1,  # x axis
+#		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
+#		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
+#	axis( 2,  # y axis
+#		at=c(-.4,-.3,-.2,-.1,0,.1,.2,.3,.4),
+#		c("-0.4","","-0.2","","0.0","","0.2","","0.4"))
+#	
+#		
+#	points( result$r.rmsf_cr, result$r.rmsf_avg_md, pch=19 )
+#	abline(0,1)
 
 close.screen(all = TRUE)
 
