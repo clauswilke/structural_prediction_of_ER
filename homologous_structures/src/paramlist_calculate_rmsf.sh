8afc60abdd4687a5a3fd6e648cba8c8a06a49796
dariyasydykova <daria.sydykova@gmail.com>
2014-04-08 11:00:15 -0500

    Updated alignments

--- homologous_structures/src/paramlist_calculate_rmsf.sh
+++
@@ -1,6 +0,0 @@
-./calculate_rmsf.sh 1RD8 AB HP
-#./calculate_rmsf.sh 2JLY A DPH
-#./calculate_rmsf.sh 2FP7 B WNP
-#./calculate_rmsf.sh 2Z83 A JEH
-#./calculate_rmsf.sh 3GOL A HCP
-#./calculate_rmsf.sh 3LYF A RVFN
